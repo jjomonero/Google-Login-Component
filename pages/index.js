a6579b7b09024d5f566a3881965fb337b23a71a0
João Pedro de Freitas <73150115+jjomonero@users.noreply.github.com>
2021-05-24 10:44:13 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,10 @@ export class App extends Component {
         </Head>
         <main>
           <GoogleLogin
-            clientId="585461355287-25mcae0i1ur704kaek9ukuohkm7na77m.apps.googleusercontent.com"
+            // that ID you can seek in google cloud in access key oAuth, necessary  make login 
+            //for generate a only token... oAuth is an method validation as the JWT ( Javascript Web Token ) 
+            //and being thus much used for Social Login, as this case for example.
+            clientId=" "
             buttonText="Login"
             onSuccess={this.responseGoogle}
             onFailure={this.responseGoogle}
